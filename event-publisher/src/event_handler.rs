49e68a3c95f3225fc350279b2e59ff4533538eff
Vandana Varakantham <yvandana.varakantham@datacore.com>
2023-08-07 08:56:29 +0000

    feat(eventing): init with optional spawn function
    Signed-off-by: Vandana Varakantham <yvandana.varakantham@datacore.com>

--- event-publisher/src/event_handler.rs
+++ event-publisher/src/event_handler.rs
@@ -9,14 +9,26 @@ pub struct EventHandle {}
 impl EventHandle {
     /// Initilize the buffer, starts the event publisher and return the layer for handling the event
     /// traces.
-    pub fn init(mbus_url: String, service_name: &str) -> EventLayer {
+    pub fn init<T>(mbus_url: String, service_name: &str, spawn_option: Option<T>) -> EventLayer
+    where
+        T: Fn(std::pin::Pin<Box<dyn core::future::Future<Output = ()> + Send>>),
+    {
         let (send, recv) = tokio::sync::mpsc::channel::<EventMessage>(MAX_BUFFER_MSGS);
         initilize_source_component(service_name);
         let layer = EventLayer::new(send);
-        tokio::spawn(async move {
+        let publisher_future = Box::pin(async move {
             MbusPublisher::run(&mbus_url, recv).await;
             // TODO handle the closed channel situation.
         });
+        match spawn_option {
+            Some(spawn_fn) => {
+                // Spawn the publisher on the spawner specified in the args.
+                spawn_fn(publisher_future);
+            }
+            None => {
+                tokio::spawn(publisher_future);
+            }
+        }
         layer
     }
 }
