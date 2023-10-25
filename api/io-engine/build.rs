03d6ccf32bf4f78da3d97e215058ec3c4d9a4200
Tiago Castro <tiagolobocastro@gmail.com>
2023-10-25 17:08:21 +0100

    chore(api/io-engine,csi): migrate from api repo
    Signed-off-by: Tiago Castro <tiagolobocastro@gmail.com>

---
+++ api/io-engine/build.rs
@@ -0,0 +1,37 @@
+use std::{env, path::PathBuf};
+
+extern crate tonic_build;
+
+fn main() {
+    let reflection_descriptor =
+        PathBuf::from(env::var("OUT_DIR").unwrap()).join("mayastor_reflection.bin");
+    tonic_build::configure()
+        .file_descriptor_set_path(&reflection_descriptor)
+        .build_server(true)
+        .type_attribute(".", "#[derive(serde::Serialize, serde::Deserialize)]")
+        .extern_path(".google.protobuf.Timestamp", "::prost_wkt_types::Timestamp")
+        .compile(&["protobuf/mayastor.proto"], &["protobuf"])
+        .unwrap_or_else(|e| panic!("mayastor protobuf compilation failed: {}", e));
+
+    tonic_build::configure()
+        .file_descriptor_set_path(&reflection_descriptor)
+        .build_server(true)
+        .type_attribute(".", "#[derive(serde::Serialize, serde::Deserialize)]")
+        .extern_path(".google.protobuf.Timestamp", "::prost_wkt_types::Timestamp")
+        .extern_path(".google.protobuf.Duration", "::prost_wkt_types::Duration")
+        .compile(
+            &[
+                "protobuf/v1/bdev.proto",
+                "protobuf/v1/json.proto",
+                "protobuf/v1/pool.proto",
+                "protobuf/v1/replica.proto",
+                "protobuf/v1/host.proto",
+                "protobuf/v1/nexus.proto",
+                "protobuf/v1/registration.proto",
+                "protobuf/v1/snapshot.proto",
+                "protobuf/v1/test.proto",
+            ],
+            &["protobuf/v1"],
+        )
+        .unwrap_or_else(|e| panic!("mayastor v1 protobuf compilation failed: {}", e));
+}
