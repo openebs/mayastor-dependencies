4d5cb759e7af7c48111d7581380e4693aec8c2f6
Niladri Halder <niladri.halder26@gmail.com>
2024-05-10 11:25:23 +0000

    ci: add config.nix file for storing shared constants
    Signed-off-by: Niladri Halder <niladri.halder26@gmail.com>

---
+++ config.nix
@@ -0,0 +1,8 @@
+/*
+  This file contains default values for parameters used in nix derivations.
+  Set a value here if you want to share a nix value across all of the repos
+  which use this repo as a git submodule.
+*/
+{
+  product_prefix = "mayastor";
+}
