694cae5dcd9daed688896b754390bb88fd24e203
Diwakar Sharma <120022567+dsharma-dc@users.noreply.github.com>
2025-03-13 23:26:51 +0530

    chore: rename crate to secret-provider (#117)
    Signed-off-by: Diwakar Sharma <diwakar.sharma@datacore.com>

--- secret-provider/examples/src/main.rs
+++ secret-provider/examples/src/main.rs
@@ -35,18 +35,23 @@ pub enum Cipher {
 }
 
 #[tokio::main]
+/// Run this example from secret-provider directory.
 async fn main() -> Result<(), Error> {
+    let cwd = std::env::current_dir().unwrap();
+    println!("Encryption parameters file location: {cwd:?}");
+
+    let k8s_provider = FileSecretProvider::new(cwd);
+    let raw_secret: EncryptionParameters = k8s_provider
+        .secret_data("example_secret_params.json")
+        .await?;
+    println!("Encryption Parameters from file: {:?}", raw_secret);
+
     let client = kube::Client::try_default()
         .await
         .expect("Should be able to create kube client");
     let k8s_provider = K8sSecretProvider::new(client, "default");
     let raw_secret: EncryptionParameters = k8s_provider.secret_data("my-secret").await?;
     println!("Encryption Parameters from k8s: {:?}", raw_secret);
-
-    let k8s_provider =
-        FileSecretProvider::new("/root/work/openebs/mayastor-dependencies/secret_provider".into());
-    let raw_secret: EncryptionParameters = k8s_provider.secret_data("my-secret").await?;
-    println!("Encryption Parameters from file: {:?}", raw_secret);
 
     Ok(())
 }
