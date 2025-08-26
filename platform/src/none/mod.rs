d54648a60cdd8976c57081abbfb26978df2011a1
Tiago Castro <tiagolobocastro@gmail.com>
2025-08-26 13:45:37 +0100

    feat(platform): allow namespace configuration
    Signed-off-by: Tiago Castro <tiagolobocastro@gmail.com>

--- platform/src/none/mod.rs
+++ platform/src/none/mod.rs
@@ -13,6 +13,7 @@ pub(super) struct None {
 const MACHINE_ID: &str = "/etc/machine-id";
 const NOPLATFORM_UUID_VAR: &str = "NOPLATFORM_UUID";
 const DUMMY_PLATFORM_UUID: &str = "dummy-uuid";
+const NOPLATFORM_NS_VAR: &str = "NOPLATFORM_NS";
 const DUMMY_PLATFORM_NS: &str = "default";
 
 impl None {
@@ -23,10 +24,11 @@ impl None {
         } else {
             std::fs::read_to_string(MACHINE_ID).unwrap_or_else(|_| DUMMY_PLATFORM_UUID.to_string())
         };
+        let platform_ns = std::env::var(NOPLATFORM_NS_VAR).unwrap_or(DUMMY_PLATFORM_NS.to_string());
 
         Self {
             platform_uuid: platform_uuid.trim().to_string(),
-            platform_ns: PlatformNS::from(DUMMY_PLATFORM_NS),
+            platform_ns: PlatformNS::from(platform_ns),
         }
     }
 }
