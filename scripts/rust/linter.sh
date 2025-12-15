20c787d7fc09af643ffa24f0d52a233015fd1c0c
Tiago Castro <tiagolobocastro@gmail.com>
2025-12-15 12:30:50 +0000

    build: use shared workspace dependencies
    This will make crate upgrades easier to maintain.
    
    Signed-off-by: Tiago Castro <tiagolobocastro@gmail.com>

--- scripts/rust/linter.sh
+++ scripts/rust/linter.sh
@@ -2,8 +2,4 @@
 
 set -euo pipefail
 
-for d in `find -maxdepth 3 -name Cargo.toml -printf '%h\n' | grep -v "^./h2"`; do
-    pushd $d
-    cargo-clippy --all --all-targets -- -D warnings
-    popd
-done
+cargo-clippy --all --all-targets -- -D warnings
